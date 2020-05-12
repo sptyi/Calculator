be28586b6afae2e92273fecf4316fa84f33f2ce3
sptyi <cjhilaman@yahoo.com>
2020-05-11 21:07:45 -0400

    Framework setup.

---
+++ calc.js
@@ -0,0 +1,50 @@
+const btn0 = querySelector('#btn0');
+const btn1 = querySelector('#btn1');
+const btn2 = querySelector('#btn2');
+const btn3 = querySelector('#btn3');
+const btn4 = querySelector('#btn4');
+const btn5 = querySelector('#btn5');
+const btn6 = querySelector('#btn6');
+const btn7 = querySelector('#btn7');
+const btn8 = querySelector('#btn8');
+const btn9 = querySelector('#btn9');
+const plusBtn = querySelector('#plusBtn');
+const minusBtn = querySelector('#minusBtn');
+const multiplyBtn = querySelector('#multiplyBtn');
+const divideBtn = querySelector('#divideBtn');
+const equalsBtn = querySelector('#equalsBtn');
+const clearBtn = querySelector('#clearBtn');
+var nums = [];
+var operators = [];
+
+function add(nums) {
+	let result = ;
+	return result;
+}
+
+function subtract(nums) {
+	let result = ;
+	return result;
+}
+
+function multiply(nums) {
+	let result = ;
+	return result;
+}
+
+function divide(nums) {
+	let result = ;
+	return result;
+}
+
+function operate(operators, nums) {
+	if (operators = 'add') {
+		add(nums);
+	} else if(operators = 'subtract') {
+		subtract(nums);
+		} else if(operators = 'multiply') {
+			multiply(nums);
+		} else {
+			divide(nums);
+		}
+}

978496b2f75c9ca209765b8a8fce410041370baf
CJ Hilaman <cjhilaman@yahoo.com>
2020-06-04 03:35:44 +0000

    Add functional positive/negative and percent buttons.

--- calc.js
+++ calc.js
@@ -15,6 +15,8 @@ const divideBtn = document.querySelector('#divideBtn');
 const equalsBtn = document.querySelector('#equalsBtn');
 const clearBtn = document.querySelector('#clearBtn');
 const decimalBtn = document.querySelector('#decimalBtn');
+const negativePositiveBtn = document.querySelector('#negativePositiveBtn');
+const percentageBtn = document.querySelector('#percentageBtn');
 const backspaceBtn = document.querySelector('#backspaceBtn');
 const display = document.querySelector('#display');
 
@@ -24,6 +26,8 @@ var addCall = false;
 var subtractCall = false;
 var multiplyCall = false;
 var divideCall = false;
+var percentageCall = false;
+var nextNumNegative = false;
 var calcResult;
 var expoResult;
 var keypressCode;
@@ -40,6 +44,10 @@ btn0.addEventListener('click', () => {
 btn1.addEventListener('click', () => {
 	if (num.length >= 9) {
 		return;
+	} else if (nextNumNegative) {
+		num = 1 * -1
+		display.textContent = num;
+		nextNumNegative = false;
 	} else {
 		num += '1';
 		display.textContent = num;
@@ -49,6 +57,10 @@ btn1.addEventListener('click', () => {
 btn2.addEventListener('click', () => {
 	if (num.length >= 9) {
 		return;
+	} else if (nextNumNegative) {
+		num = 2 * -1
+		display.textContent = num;
+		nextNumNegative = false;
 	} else {
 		num += '2';
 		display.textContent = num;
@@ -58,6 +70,10 @@ btn2.addEventListener('click', () => {
 btn3.addEventListener('click', () => {
 	if (num.length >= 9) {
 		return;
+	} else if (nextNumNegative) {
+		num = 3 * -1
+		display.textContent = num;
+		nextNumNegative = false;
 	} else {
 		num += '3';
 		display.textContent = num;
@@ -67,6 +83,10 @@ btn3.addEventListener('click', () => {
 btn4.addEventListener('click', () => {
 	if (num.length >= 9) {
 		return;
+	} else if (nextNumNegative) {
+		num = 4 * -1
+		display.textContent = num;
+		nextNumNegative = false;
 	} else {
 		num += '4';
 		display.textContent = num;
@@ -76,6 +96,10 @@ btn4.addEventListener('click', () => {
 btn5.addEventListener('click', () => {
 	if (num.length >= 9) {
 		return;
+	} else if (nextNumNegative) {
+		num = 5 * -1
+		display.textContent = num;
+		nextNumNegative = false;
 	} else {
 		num += '5';
 		display.textContent = num;
@@ -85,6 +109,10 @@ btn5.addEventListener('click', () => {
 btn6.addEventListener('click', () => {
 	if (num.length >= 9) {
 		return;
+	} else if (nextNumNegative) {
+		num = 6 * -1
+		display.textContent = num;
+		nextNumNegative = false;
 	} else {
 		num += '6';
 		display.textContent = num;
@@ -94,6 +122,10 @@ btn6.addEventListener('click', () => {
 btn7.addEventListener('click', () => {
 	if (num.length >= 9) {
 		return;
+	} else if (nextNumNegative) {
+		num = 7 * -1
+		display.textContent = num;
+		nextNumNegative = false;
 	} else {
 		num += '7';
 		display.textContent = num;
@@ -103,6 +135,10 @@ btn7.addEventListener('click', () => {
 btn8.addEventListener('click', () => {
 	if (num.length >= 9) {
 		return;
+	} else if (nextNumNegative) {
+		num = 8 * -1
+		display.textContent = num;
+		nextNumNegative = false;
 	} else {
 		num += '8';
 		display.textContent = num;
@@ -112,6 +148,10 @@ btn8.addEventListener('click', () => {
 btn9.addEventListener('click', () => {
 	if (num.length >= 9) {
 		return;
+	} else if (nextNumNegative) {
+		num = 9 * -1
+		display.textContent = num;
+		nextNumNegative = false;
 	} else {
 		num += '9';
 		display.textContent = num;
@@ -149,12 +189,14 @@ clearBtn.addEventListener('click', () => {
 	subtractCall = false;
 	multiplyCall = false;
 	divideCall = false;
+	percentageCall = false;
+	nextNumNegative = false;
 	calcResult = '';
 	display.textContent = 0;
 });
 
 decimalBtn.addEventListener('click', () => {
-	if (num.includes('.') == false) {
+	if (!num.includes('.')) {
 		if (!num) {
 			num += '0.';
 			display.textContent = num;
@@ -163,6 +205,19 @@ decimalBtn.addEventListener('click', () => {
 			display.textContent = num;
 		}
 	}
+});
+
+negativePositiveBtn.addEventListener('click', () => {
+	if (num == 0 || subtractCall || addCall || multiplyCall || divideCall) {
+		nextNumNegative = true;
+	} else {
+		num = num * -1;
+		display.textContent = num;
+	}
+});
+
+percentageBtn.addEventListener('click', () => {
+	computePercentage();
 });
 
 backspaceBtn.addEventListener('click', () => {
@@ -192,137 +247,198 @@ addEventListener('keydown', () => {
 		}
 	} else if (keypressCode == 97 || keypressCode == 49) {
 		if (num.length <= 9) {
-			num += '1';
-			display.textContent = num;
-			btn1.style.cssText =
-				'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
-			setTimeout(() => {
+			if (nextNumNegative) {
+				num = 1 * -1
+				display.textContent = num;
+				nextNumNegative = false;
+			} else {
+				num += '1';
+				display.textContent = num;
 				btn1.style.cssText =
-					'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
-			}, 100);
+					'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
+				setTimeout(() => {
+					btn1.style.cssText =
+						'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
+				}, 100);
+			}
 		}
 	} else if (keypressCode == 98 || keypressCode == 50) {
 		if (num.length <= 9) {
-			num += '2';
-			display.textContent = num;
-			btn2.style.cssText =
-				'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
-			setTimeout(() => {
+			if (nextNumNegative) {
+				num = 2 * -1
+				display.textContent = num;
+				nextNumNegative = false;
+			} else {
+				num += '2';
+				display.textContent = num;
 				btn2.style.cssText =
-					'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
-			}, 100);
+					'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
+				setTimeout(() => {
+					btn2.style.cssText =
+						'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
+				}, 100);
+			}
 		}
 	} else if (keypressCode == 99 || keypressCode == 51) {
 		if (num.length <= 9) {
-			num += '3';
-			display.textContent = num;
-			btn3.style.cssText =
-				'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
-			setTimeout(() => {
+			if (nextNumNegative) {
+				num = 3 * -1
+				display.textContent = num;
+				nextNumNegative = false;
+			} else {
+				num += '3';
+				display.textContent = num;
 				btn3.style.cssText =
-					'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
-			}, 100);
+					'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
+				setTimeout(() => {
+					btn3.style.cssText =
+						'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
+				}, 100);
+			}
 		}
 	} else if (keypressCode == 100 || keypressCode == 52) {
 		if (num.length <= 9) {
-			num += '4';
-			display.textContent = num;
-			btn4.style.cssText =
-				'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
-			setTimeout(() => {
+			if (nextNumNegative) {
+				num = 4 * -1
+				display.textContent = num;
+				nextNumNegative = false;
+			} else {
+				num += '4';
+				display.textContent = num;
 				btn4.style.cssText =
-					'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
-			}, 100);
+					'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
+				setTimeout(() => {
+					btn4.style.cssText =
+						'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
+				}, 100);
+			}
 		}
-	} else if (keypressCode == 101 || keypressCode == 53) {
+	} else if (keypressCode == 101 || keypressCode == 53 && !event.shiftKey) {
 		if (num.length <= 9) {
-			num += '5';
-			display.textContent = num;
-			btn5.style.cssText =
-				'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
-			setTimeout(() => {
+			if (nextNumNegative) {
+				num = 5 * -1
+				display.textContent = num;
+				nextNumNegative = false;
+			} else {
+				num += '5';
+				display.textContent = num;
 				btn5.style.cssText =
-					'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
-			}, 100);
+					'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
+				setTimeout(() => {
+					btn5.style.cssText =
+						'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
+				}, 100);
+			}
 		}
 	} else if (keypressCode == 102 || keypressCode == 54) {
 		if (num.length <= 9) {
-			num += '6';
-			display.textContent = num;
-			btn6.style.cssText =
-				'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
-			setTimeout(() => {
+			if (nextNumNegative) {
+				num = 6 * -1
+				display.textContent = num;
+				nextNumNegative = false;
+			} else {
+				num += '6';
+				display.textContent = num;
 				btn6.style.cssText =
-					'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
-			}, 100);
+					'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
+				setTimeout(() => {
+					btn6.style.cssText =
+						'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
+				}, 100);
+			}
 		}
 	} else if (keypressCode == 103 || keypressCode == 55) {
 		if (num.length <= 9) {
-			num += '7';
-			display.textContent = num;
-			btn7.style.cssText =
-				'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
-			setTimeout(() => {
+			if (nextNumNegative) {
+				num = 7 * -1
+				display.textContent = num;
+				nextNumNegative = false;
+			} else {
+				num += '7';
+				display.textContent = num;
 				btn7.style.cssText =
-					'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
-			}, 100);
+					'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
+				setTimeout(() => {
+					btn7.style.cssText =
+						'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
+				}, 100);
+			}
 		}
-	} else if (keypressCode == 104 || keypressCode == 56) {
+	} else if (keypressCode == 104 || keypressCode == 56 && !event.shiftKey) {
 		if (num.length <= 9) {
-			num += '8';
-			display.textContent = num;
-			btn8.style.cssText =
-				'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
-			setTimeout(() => {
+			if (nextNumNegative) {
+				num = 8 * -1
+				display.textContent = num;
+				nextNumNegative = false;
+			} else {
+				num += '8';
+				display.textContent = num;
 				btn8.style.cssText =
-					'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
-			}, 100);
+					'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
+				setTimeout(() => {
+					btn8.style.cssText =
+						'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
+				}, 100);
+			}
 		}
 	} else if (keypressCode == 105 || keypressCode == 57) {
 		if (num.length <= 9) {
-			num += '9';
-			display.textContent = num;
-			btn9.style.cssText =
-				'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
-			setTimeout(() => {
+			if (nextNumNegative) {
+				num = 9 * -1
+				display.textContent = num;
+				nextNumNegative = false;
+			} else {
+				num += '9';
+				display.textContent = num;
 				btn9.style.cssText =
-					'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
-			}, 100);
+					'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
+				setTimeout(() => {
+					btn9.style.cssText =
+						'border: 2px solid #ccc; box-shadow: 2px 2px 5px black;';
+				}, 100);
+			}
 		}
-	} else if (keypressCode == 107) {
+	} else if (keypressCode == 107 || keypressCode == 187 && event.shiftKey) {
 		add();
 		plusBtn.style.cssText =
 			'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
 		setTimeout(() => {
 			plusBtn.style.cssText = 'box-shadow: 2px 2px 5px black;';
 		}, 100);
-	} else if (keypressCode == 106) {
+	} else if (keypressCode == 106 || keypressCode == 56 && event.shiftKey) {
 		multiply();
 		multiplyBtn.style.cssText =
 			'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
 		setTimeout(() => {
 			multiplyBtn.style.cssText = 'box-shadow: 2px 2px 5px black;';
 		}, 100);
-	} else if (keypressCode == 109) {
+	} else if (keypressCode == 109 || keypressCode == 189) {
 		subtract();
 		minusBtn.style.cssText =
 			'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
 		setTimeout(() => {
 			minusBtn.style.cssText = 'box-shadow: 2px 2px 5px black;';
 		}, 100);
-	} else if (keypressCode == 111) {
+	} else if (keypressCode == 111 || keypressCode == 191) {
 		divide();
 		divideBtn.style.cssText =
 			'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
 		setTimeout(() => {
 			divideBtn.style.cssText = 'box-shadow: 2px 2px 5px black;';
 		}, 100);
-	} else if (keypressCode == 13) {
+	} else if (keypressCode == 13 || keypressCode == 187 && !event.shiftKey) {
 		operate();
 		equalsBtn.style.cssText =
 			'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
 		setTimeout(() => {
 			equalsBtn.style.cssText = 'box-shadow: 2px 2px 5px black;';
+		}, 100);
+	} else if (keypressCode == 53 && event.shiftKey ) {
+		computePercentage();
+		percentageBtn.style.cssText =
+			'border: none; box-shadow: none; transform: translate(3px, 3px) scale(0.999, 0.999)';
+		setTimeout(() => {
+			percentageBtn.style.cssText = 'box-shadow: 2px 2px 5px black;';
 		}, 100);
 	} else if (keypressCode == 27) {
 		num = '';
@@ -331,6 +447,8 @@ addEventListener('keydown', () => {
 		subtractCall = false;
 		multiplyCall = false;
 		divideCall = false;
+		percentageCall = false;
+		nextNumNegative = false;
 		calcResult = '';
 		display.textContent = 0;
 		clearBtn.style.cssText =
@@ -339,7 +457,7 @@ addEventListener('keydown', () => {
 			clearBtn.style.cssText = 'box-shadow: 2px 2px 5px black;';
 		}, 100);
 	} else if (keypressCode == 110) {
-		if (num.includes('.') == false) {
+		if (!num.includes('.')) {
 			if (!num) {
 				num += '0.';
 				display.textContent = num;
@@ -376,7 +494,7 @@ function add() {
 	if (addCall || subtractCall || multiplyCall || divideCall) {
 		operate();
 	}
-	if (addCall == false) {
+	if (!addCall) {
 		oldNum = '';
 		oldNum += num;
 		num = '';
@@ -388,7 +506,7 @@ function subtract() {
 	if (subtractCall || addCall || multiplyCall || divideCall) {
 		operate();
 	}
-	if (subtractCall == false) {
+	if (!subtractCall) {
 		oldNum = '';
 		oldNum += num;
 		num = '';
@@ -400,7 +518,7 @@ function multiply() {
 	if (multiplyCall || subtractCall || addCall || divideCall) {
 		operate();
 	}
-	if (multiplyCall == false) {
+	if (!multiplyCall) {
 		oldNum = '';
 		oldNum += num;
 		num = '';
@@ -412,7 +530,7 @@ function divide() {
 	if (divideCall || subtractCall || multiplyCall || addCall) {
 		operate();
 	}
-	if (divideCall == false) {
+	if (!divideCall) {
 		oldNum = '';
 		oldNum += num;
 		num = '';
@@ -420,8 +538,20 @@ function divide() {
 	}
 }
 
+function computePercentage() {
+	if (divideCall || subtractCall || multiplyCall || addCall) {
+		operate();
+	}
+	if (!percentageCall) {
+		oldNum = '';
+		oldNum += num;
+		num = '';
+		percentageCall = true;
+	}
+}
+
 function operate() {
-	if (addCall == true) {
+	if (addCall ) {
 		calcResult = Number(oldNum) + Number(num);
 		if (calcResult.toString().length >= 9) {
 			expoResult = calcResult.toExponential(3);
@@ -434,7 +564,7 @@ function operate() {
 		}
 		oldNum = '';
 		addCall = false;
-	} else if (subtractCall == true) {
+	} else if (subtractCall ) {
 		calcResult = Number(oldNum) - Number(num);
 		if (calcResult.toString().length >= 9) {
 			expoResult = calcResult.toExponential(3);
@@ -447,7 +577,7 @@ function operate() {
 		}
 		oldNum = '';
 		subtractCall = false;
-	} else if (multiplyCall == true) {
+	} else if (multiplyCall ) {
 		calcResult = Number(oldNum) * Number(num);
 		if (calcResult.toString().length >= 9) {
 			expoResult = calcResult.toExponential(3);
@@ -460,8 +590,16 @@ function operate() {
 		}
 		oldNum = '';
 		multiplyCall = false;
-	} else if (divideCall == true && num != 0) {
+	} else if (divideCall  && num != 0) {
 		calcResult = Number(oldNum) / Number(num);
+		if (calcResult.toString().length >= 9) {
+			expoResult = calcResult.toExponential(3);
+			display.textContent = expoResult;
+			num = expoResult;
+			expoResult = '';
+		}
+	} else if (percentageCall) {
+		calcResult = Number(oldNum) / Number(num) * 100;
 		if (calcResult.toString().length >= 9) {
 			expoResult = calcResult.toExponential(3);
 			display.textContent = expoResult;
@@ -473,7 +611,7 @@ function operate() {
 		}
 		oldNum = '';
 		divideCall = false;
-	} else if (divideCall == true && num == 0) {
+	} else if (divideCall  && num == 0) {
 		display.textContent = 'But...how?';
 	}
 }
